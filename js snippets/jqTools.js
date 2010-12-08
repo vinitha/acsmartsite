ad15e0b3db84871da97a08e4913afad8b6bf1bb1
alessio.carnevale <alessio.carnevale@gmail.com>
2010-12-08 14:45:21 +0000

    fixed the aleCarousle module

--- js snippets/jqTools.js
+++ js snippets/jqTools.js
@@ -961,6 +961,7 @@ var utils={
 
 
 
+// remap jQuery to $
 (function($){
     $.fn.aleCarousel=function(options){
 	var el=this;
@@ -970,6 +971,7 @@ var utils={
 		duration:2000,
 		speed:1200,
 		autostart:false,
+                ieFix:($.browser.msie && parseInt($.browser.version)<7)?3:0,
 		events:{
 		    start:function(){},
 		    stop:function(){},
@@ -979,7 +981,7 @@ var utils={
 		}
 	};
 	//extending the default options
-	$.extend(defaults,options)
+	$.extend(defaults,options);
 	defaults.transitionFx=eval(defaults.transitionFx);
 	
 	var intervalHnd=null,
@@ -994,54 +996,60 @@ var utils={
 			position:"absolute",
 			width:"100%",
 			display:"none"
-		})
-	    	 
+		});
+                
 	//making a note of the panel index
-	LIs.each(function(i){$(this).data("index",i)})
+	LIs.each(function(i){$(this).data("index",i);});
 		
 	var pluginObj={
 	    $objects:el,
 	    currentPanel:null,
 	    nextPanel:null,
 	    start:function(){
+                        if(LIs.length<2){return false;}
 			if (!intervalHnd){										
 				intervalHnd=setInterval(function(){defaults.transitionFx.call(pluginObj)},defaults.duration);								
 			}
 			
 			defaults.events.start.call(pluginObj);		
-			return pluginObj
+			return pluginObj;
 	    },
 	    pause:function(){
 			clearInterval(intervalHnd);	
 			intervalHnd=null;
 			
 			defaults.events.pause.call(pluginObj);
-			return pluginObj
+			return pluginObj;
 	    },
 	    goTo:function(panelIndex,trFx){
 		
-			if (pluginObj.currentPanel.data("index")==panelIndex) return pluginObj;
+			if (pluginObj.currentPanel.data("index")==panelIndex){return pluginObj;}			
 			
 			pluginObj.currentPanel.stop(true,true);
 			pluginObj.nextPanel.stop(true,true);
 			ul.stop(true,true);
 			
-			pluginObj.nextPanel=LIs.eq(panelIndex);
+			
+			if (typeof panelIndex=="object"){
+			    pluginObj.nextPanel=panelIndex;
+			}else{
+			    pluginObj.nextPanel=LIs.eq(panelIndex);   
+			}
 			if(trFx){
 				trFx=eval(trFx);
 				trFx.call(pluginObj);
 			}else{
 				defaults.transitionFx.call(pluginObj);
 			}
-			return pluginObj
+			return pluginObj;
 	    },
 	    next:function(){
 			pluginObj.currentPanel.stop(true,true);
 			pluginObj.nextPanel.stop(true,true);							
 			ul.stop(true,true);
 			
-			defaults.events.transitionFx.call(pluginObj);
-			return pluginObj
+			defaults.transitionFx.call(pluginObj);
+			return pluginObj;
 	    },
 	    previous:function(){
 			pluginObj.nextPanel=this.currentPanel.prev().length>0?this.currentPanel.prev():this.currentPanel.nextAll(":last");
@@ -1050,9 +1058,8 @@ var utils={
 			pluginObj.nextPanel.stop(true,true);	
 			ul.stop(true,true);
 			
-			defaults.transitionFx.call(pluginObj);
-			
-			return pluginObj
+			defaults.transitionFx.call(pluginObj);			
+			return pluginObj;
 	    }
 	};
 	
@@ -1060,10 +1067,10 @@ var utils={
 	pluginObj.nextPanel=pluginObj.currentPanel.next().length>0?pluginObj.currentPanel.next():pluginObj.currentPanel.prevAll(":last");
 	
 	ul.css("height","auto");
+
+	if(defaults.autostart && LIs.length>1){pluginObj.start();}
 	
-	if(defaults.autostart){pluginObj.start()}
-	
-	return pluginObj
+	return pluginObj;
     
     
     
@@ -1079,36 +1086,36 @@ var utils={
 		    .show(0);
 							    
 	    var viewportHeight=ul.height();
-		var nextPHeight=this.nextPanel.height();
-	    ul.animate({"height":this.nextPanel.height()},defaults.speed);	    	    
+	    var nextPHeight=this.nextPanel.height();
+            
+	    ul.animate({"height":this.nextPanel.innerHeight(true)+defaults.ieFix},defaults.speed);            
+    
 	    
-	    this.currentPanel.css("position","absolute")
+	    this.currentPanel.css("position","absolute");
 		
 		if(this.nextPanel.data("index")<this.currentPanel.data("index")){
 			this.nextPanel.css("top",-nextPHeight);
 			
-			this.currentPanel.animate({"top":nextPHeight},defaults.speed,function(){$(this).css("z-index",0).hide(0)})
+			this.currentPanel.animate({"top":nextPHeight},defaults.speed,function(){$(this).css("z-index",0).hide(0);});
 			this.nextPanel.animate({"top":0},defaults.speed,function(){
 				defaults.events.change.call(thisObj);
-				thisObj.currentPanel.css("position","static")
-				ul.css("height","auto")
-			})										
+				thisObj.currentPanel.css("position","static");
+				ul.css("height","auto");
+			});										
 		}else{
 			this.nextPanel.css("top",ul.height());
 			
-			this.currentPanel.animate({"top":-viewportHeight},defaults.speed,function(){$(this).css("z-index",0).hide(0)})
+			this.currentPanel.animate({"top":-viewportHeight},defaults.speed,function(){$(this).css("z-index",0).hide(0);});
 			this.nextPanel.animate({"top":0},defaults.speed,function(){
 				defaults.events.change.call(thisObj);
-				thisObj.currentPanel.css("position","static")
-				ul.css("height","auto")
-			})										
+				thisObj.currentPanel.css("position","static");
+				ul.css("height","auto");
+			});
 		}
-		
-					
 	    
 	    //setting the new current and next panel
 	    this.currentPanel=this.nextPanel;			
-	    this.nextPanel=this.currentPanel.next().length>0?this.currentPanel.next():this.currentPanel.prevAll(":last")
+	    this.nextPanel=this.currentPanel.next().length>0?this.currentPanel.next():this.currentPanel.prevAll(":last");
 																    
 	}
 						
@@ -1118,24 +1125,22 @@ var utils={
 	    defaults.events.beforeChange.call(this);
 	    this.currentPanel.css("z-index",1);
 	    this.nextPanel.css("z-index",2);
-	    
-	    var viewportWidth=this.nextPanel.height();
-	    ul.animate({"height":this.nextPanel.height()},defaults.speed);
+            
+	    ul.animate({"height":this.nextPanel.innerHeight(true)+defaults.ieFix},defaults.speed);
 	    
 	    this.currentPanel.css("position","absolute");
-	    this.currentPanel.fadeOut(600,function(){$(this).css("z-index",0)})
+	    this.currentPanel.fadeOut(600,function(){$(this).css("z-index",0);});
 	    this.nextPanel.fadeIn(defaults.speed,function(){
 			defaults.events.change.call(thisObj);
-			thisObj.currentPanel.css("position","static")
-			ul.css("height","auto")
-	    })						
+			thisObj.currentPanel.css("position","static");
+			ul.css("height","auto");
+	    });
 	    
 	    //setting the new current and next panel
 	    this.currentPanel=this.nextPanel;			
-	    this.nextPanel=this.currentPanel.next().length>0?this.currentPanel.next():this.currentPanel.prevAll(":last")
-
-	}					
-	
+	    this.nextPanel=this.currentPanel.next().length>0?this.currentPanel.next():this.currentPanel.prevAll(":last");
+	}
+        
 	function swap(){
 	    var thisObj=this;
 	    
@@ -1147,51 +1152,49 @@ var utils={
 							    
 	    var viewportWidth=this.nextPanel.height();
 	    
-	    ul.animate({"height":this.nextPanel.height()},defaults.speed);
+	    ul.animate({"height":this.nextPanel.innerHeight(true)+defaults.ieFix},defaults.speed);
 	    
 	    this.nextPanel.css("top",-viewportWidth);
 	    this.currentPanel.css("position","absolute");
-	    this.currentPanel.animate({"top":-viewportWidth},defaults.speed,function(){$(this).css("z-index",0).hide(0)})
+	    this.currentPanel.animate({"top":-viewportWidth},defaults.speed,function(){$(this).css("z-index",0).hide(0)});
 	    this.nextPanel.animate({"top":0},defaults.speed,function(){
 			defaults.events.change.call(thisObj);
-			thisObj.currentPanel.css("position","static")
-			ul.css("height","auto")
-	    })						
+			thisObj.currentPanel.css("position","static");
+			ul.css("height","auto");
+	    });						
 	    
 	    //setting the new current and next panel
 	    this.currentPanel=this.nextPanel;			
-	    this.nextPanel=this.currentPanel.next().length>0?this.currentPanel.next():this.currentPanel.prevAll(":last")
+	    this.nextPanel=this.currentPanel.next().length>0?this.currentPanel.next():this.currentPanel.prevAll(":last");
 
 	}
 	
 	function snap(){	    
 	    defaults.events.beforeChange.call(this);
 	    
-	    ul.css("height","auto")
+	    ul.css("height","auto");
 	    
 	    this.currentPanel.css("z-index",1);
 	    this.nextPanel.css("z-index",2);
 	    
 	    this.currentPanel.css("position","absolute");
 	    var viewportWidth=this.nextPanel.height();
-	   	    
 	    
-	    this.currentPanel.hide(0)
-	    this.currentPanel.css("z-index",0)
-	    this.nextPanel.show(0)
+	    this.currentPanel.hide(0);
+	    this.currentPanel.css("z-index",0);
+	    this.nextPanel.show(0);
 	    
 	    
 	    
 	    //setting the new current and next panel
 	    this.currentPanel=this.nextPanel;		
-	    this.nextPanel=this.currentPanel.next().length>0?this.currentPanel.next():this.currentPanel.prevAll(":last")
+	    this.nextPanel=this.currentPanel.next().length>0?this.currentPanel.next():this.currentPanel.prevAll(":last");
 	    
-	    this.currentPanel.css("position","static")
+	    this.currentPanel.css("position","static");
 	    defaults.events.change.call(this);
 	}	    
-    }
-    
-})(jQuery);
+    };
+})(window.jQuery);
 
 
 //ixCarousel
