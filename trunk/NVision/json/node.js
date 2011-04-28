383c022d3493a3172f41f68892c7bf006f2a7baf
alessio.carnevale@gmail.com <alessio.carnevale@gmail.com@b75131de-78fc-11de-a93f-2d52da730ff4>
2011-04-28 14:05:52 +0000

    - "items per page" dropdown - allow the user to resubmit the same search more than one time - Test performances with real data (delays in the tableView refresh creates inconsistencies) - implement scroll buttons to the pagination widget - improving pagination performance

--- trunk/NVision/json/node.js
+++
@@ -1,3 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {res.writeHead(200, {'Content-Type': 'application/json'});		res.end('Hello World\n');		}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
